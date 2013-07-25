20fe623b6daee91a0d2c89414574c9533773218b
Marcin Pewiński <marcin.pewinski@ansta.pl>
2013-07-25 23:36:00 +0200

    ...

--- loggedin.php
+++ loggedin.php
@@ -6,6 +6,7 @@
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     <title>Auth Demo</title>
 		<script src="phonegap.js" type="text/javascript" charset="utf-8"></script>
+    <script src="js/jquery-1.8.3.js"></script>
     <script>
       $.post('http://ap-test.d.d.ansta.pl/loggedin.php',{}, function(resp) {
         $('body').html(resp);

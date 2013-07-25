b2ed3627c692ffa4fbbeb6d385cb9848932639ec
Marcin Pewiński <marcin.pewinski@ansta.pl>
2013-07-25 23:18:02 +0200

    .

--- loggedin.php
+++ loggedin.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=320; user-scalable=no" />
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     <title>Auth Demo</title>
+		<script src="phonegap.js" type="text/javascript" charset="utf-8"></script>
 </head>
 
 <body>
